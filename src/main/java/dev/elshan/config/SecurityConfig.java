c3ba231db16028a7e9e28adb4ea37b01d3f61bfc
ElshanDunyamaliyev <elshandunyamaliyev@gmail.com>
2024-06-14 05:10:48 +0400

    added new users with inMemoryUserDetailsManager

--- src/main/java/dev/elshan/config/SecurityConfig.java
+++ src/main/java/dev/elshan/config/SecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +23,17 @@ public class SecurityConfig {
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
+    }
+
+    // Butun requestleri deny edir
+    // requests.anyRequest().denyAll();
+    // Butun requestleri permit edir
+    // requests.anyRequest().permitAll();
+
+    @Bean
+    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("12345").authorities("admin").build();
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("12345").authorities("read").build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
